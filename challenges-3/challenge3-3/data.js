ddcc40f841f632ca0411542402bebb87c53eb6eb
Renan Souza <renancjsouza@gmail.com>
2020-09-05 15:42:03 -0300

    Added challenge 3 of course Launchbase of Rocketseat

---
+++ challenges-3/challenge3-3/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: "starter",
+        course: "Starter",
+        logo: "/images/starter.svg",
+        modules: "11 Modules",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        course: "Launchbase",
+        logo: "/images/launchbase.svg",
+        modules: "52 Modules",
+        price: "Paid"
+    },
+    {
+        id: "gostack",
+        course: "GoStack",
+        logo: "/images/gostack.svg",
+        modules: "97 Modules",
+        price: "Paid"
+    }
+]
---
+++ challenges-3/challenge3-3/server.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const courses = require('./data')
+
+const server = express()
+
+server.set("view engine", "njk")
+
+server.use(express.static("public"))
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res) {    
+    return res.render("index")
+})
+
+server.get("/about", function(req, res) {
+    const about = {
+        avatar: "/images/logo-rocketseat.png",
+        brand: "Rocketseat",
+        title: "The best technologies in programming, straight to the point and in the right way.",
+        resume: "In the midst of so much information and the amount of tools that come up every day, you need someone to take you in the right direction.",
+        description: "100% free online courses to get you on the right foot in the most desired technologies on the market.",
+        technologies: [
+            {
+                technology: "Javascript",
+                image: "/images/javascript.svg"
+            },
+            {
+                technology: "Javascript ES6+",
+                image: "/images/javascript-es6.svg"
+            },
+            {
+                technology: "NodeJS",
+                image: "/images/nodejs.svg"
+            },
+            {
+                technology: "ReactJS",
+                image: "/images/reactjs.svg"
+            },
+            {
+                technology: "React Native",
+                image: "/images/react-native.svg"
+            }
+        ],
+        socialNetworks: [
+            {
+                name: "GitHub",
+                url: "https://github.com/rocketseat"
+            },
+            {
+                name: "Instagram",
+                url: "https://www.instagram.com/rocketseat_oficial"
+            },
+            {
+                name: "Facebook",
+                url: "http://fb.com/rocketseat"
+            }
+        ]
+
+    }
+    
+    return res.render("about", { about })
+})
+
+server.get("/courses", function(req, res) {
+    return res.render("courses", { items : courses })
+})
+
+server.get("/courses/:id", function(req, res) {
+    const id = req.params.id
+
+    const course = courses.find(function(course) {
+        return course.id == id
+    })
+
+    if(!course) {
+        return res.send("Course not found!")
+    }
+
+    return res.render("course", { item : course })
+})
+
+server.use(function(req, res) {
+    res.status(400).render("not-found")
+})
+
+server.listen("5000", function() {
+    console.log("Server is runing.")
+})
