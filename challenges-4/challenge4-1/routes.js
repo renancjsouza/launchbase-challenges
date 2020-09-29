56170a4d8b5730ec4db11693a3c75aa85d5cc135
Renan Souza <renancjsouza@gmail.com>
2020-09-29 00:11:13 -0300

    Updated routes

--- challenges-4/challenge4-1/routes.js
+++ challenges-4/challenge4-1/routes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const routes = express.Router();
 
-server.get('/', function(req, res) {
+routes.get('/', function(req, res) {
     return res.render('home');
 });
 
-server.get('/teachers', function(req, res) {
+routes.get('/teachers', function(req, res) {
     return res.render('teachers/index');
 });
 
-server.get('/students', function(req, res) {
+routes.get('/students', function(req, res) {
     return res.render('students/index');
 });
 
-server.use(function(req, res) {
+routes.use(function(req, res) {
     res.status(400).render('not-found');
 });
 
