c4a0ddd6e0a57d09ca90962fb8c54b19b9bac4a1
Renan Souza <renancjsouza@gmail.com>
2020-09-29 22:32:44 -0300

    Redirect route /

--- challenges-4/challenge4-1/routes.js
+++ challenges-4/challenge4-1/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', function(req, res) {
-    return res.render('home');
+    return res.render('/teachers');
 });
 
 routes.get('/teachers', function(req, res) {
